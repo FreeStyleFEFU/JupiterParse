7872238e4603f27864775cedd0f51fb5eb49954e
Михаил Гусев <gusev-miha2011@mail.ru>
2021-10-13 13:05:05 +1000

    Add graphics

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def quantity_for_every_year(total_data_frame):
     print('Сумма младенцев каждого пола для каждого года')
     print(quantity_for_every_year)
 
-    quantity_for_every_year[quantity_for_every_year['sex'] == 'F'].drop('sex', axis=1).plot(x='year', y='quantity')
-    quantity_for_every_year[quantity_for_every_year['sex'] == 'M'].drop('sex', axis=1).plot(x='year', y='quantity')
+    quantity_for_every_year[quantity_for_every_year['sex'] == 'F'].drop('sex', axis=1).plot(x='year', y='quantity', label='Female')
+    quantity_for_every_year[quantity_for_every_year['sex'] == 'M'].drop('sex', axis=1).plot(x='year', y='quantity', label='Male')
 
     plt.show()
 
@@ -40,6 +40,30 @@ def name_proportion(total_data_frame):
 
     print('Доля данного имени от общего количества')
     print(name_proportion)
+    return  total_quantity
+
+
+def jonny_natalie_bob_graphics(total_data_frame, total_quantity):
+    total_data_frame_without_sex = total_data_frame.drop('sex', axis=1)
+
+    jonny_data_frame = total_data_frame_without_sex[total_data_frame_without_sex['name'] == 'Jonny']
+    natalie_data_frame = total_data_frame_without_sex[total_data_frame_without_sex['name'] == 'Natalie']
+    bob_data_frame = total_data_frame_without_sex[total_data_frame_without_sex['name'] == 'Bob']
+
+    jonny_data_frame.plot(x='year', y='quantity', label='Jonny quantity')
+    natalie_data_frame.plot(x='year', y='quantity', label='Natalie quantity')
+    bob_data_frame.plot(x='year', y='quantity', label='Bob quantity')
+
+    jonny_data_frame['proportion'] = jonny_data_frame['quantity']/total_quantity
+    natalie_data_frame['proportion'] = natalie_data_frame['quantity']/total_quantity
+    bob_data_frame['proportion'] = bob_data_frame['quantity']/total_quantity
+
+    jonny_data_frame.drop('quantity', axis=1).plot(x='year', y='proportion', label='Jonny proportion')
+    natalie_data_frame.drop('quantity', axis=1).plot(x='year', y='proportion', label='Natalie proportion')
+    bob_data_frame.drop('quantity', axis=1).plot(x='year', y='proportion', label='Bob proportion')
+
+    plt.show()
+
 
 data_frame_1980 = babys_quantity()
 
@@ -47,4 +71,6 @@ total_data_frame = total_data_frame(data_frame_1980)
 
 quantity_for_every_year(total_data_frame)
 
-name_proportion(total_data_frame)
+total_quantity = name_proportion(total_data_frame)
+
+jonny_natalie_bob_graphics(total_data_frame, total_quantity)
